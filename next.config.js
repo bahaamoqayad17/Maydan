f5fa2cfe35c852e98ac5de2287bef8315ce2ab09
Bahaa Moqayad <bahaa200030.m7taref@gmail.com>
2023-10-04 02:40:27 +0300

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     APP_NAME: "Maydan",
-    API_URL: "http://localhost:4040/api/",
+    API_URL: "http://ewavechart.com:4041/api/",
   },
   reactStrictMode: true,
 };
