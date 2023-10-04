0dbbdb4d25850920c3168c526418c51b030a1fe3
Bahaa Moqayad <bahaa200030.m7taref@gmail.com>
2023-10-04 05:21:45 +0300

    test

--- Controllers/AdController.js
+++ Controllers/AdController.js
@@ -1,6 +1,7 @@
 const factory = require("./FactoryHandler");
 const Ad = require("../Models/Ad");
 const CatchAsync = require("../Utils/CatchAsync");
+const Application = require("../Models/Application");
 
 exports.index = factory.index(Ad);
 exports.create = CatchAsync(async (req, res, next) => {
@@ -17,3 +18,21 @@ exports.create = CatchAsync(async (req, res, next) => {
 exports.show = factory.show(Ad);
 exports.update = factory.update(Ad);
 exports.delete = factory.delete(Ad);
+
+exports.myAds = CatchAsync(async (req, res, next) => {
+  const data = await Ad.find({ company: req.user._id });
+
+  res.status(200).json({
+    status: "success",
+    data,
+  });
+});
+
+exports.adApplications = CatchAsync(async (req, res, next) => {
+  const data = await Application.find({ ad: req.params.id }).populate("user");
+
+  res.status(200).json({
+    status: "success",
+    data,
+  });
+});
--- Routes/AdRouter.js
+++ Routes/AdRouter.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const AdController = require("../Controllers/AdController");
+const AuthController = require("../Controllers/AuthController");
 
 const router = express.Router();
+
+router.use(AuthController.protect);
+
+router.get("/myAds", AuthController.restrictTo("company"), AdController.myAds);
+router.get(
+  "/adApplications/:id",
+  AuthController.restrictTo("company"),
+  AdController.adApplications
+);
 
 router.route("/").get(AdController.index).post(AdController.create);
 
